d053d648a2702e3fb2f6eee15c69832055f758d3
Jacquita Adams <adamslaquita24@gmail.com>
2020-08-26 17:06:44 -0500

    Initial Commit

---
+++ Two Buttons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Two Buttons
+//
+//  Created by MXC Swift on 8/26/20.
+//  Copyright © 2020 MXC Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
